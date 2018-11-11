f35087ff9ec72a07a2e8519e07c542259862be06
MackHsu <786188853@qq.com>
2018-11-11 18:42:04 +0800

    homework7 修改导出（序列化）的按钮上的文字

--- homework7/program2/Form1.Designer.cs
+++ homework7/program2/Form1.Designer.cs
@@ -43,19 +43,19 @@
             this.orderServiceBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.buttonSearchByID = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.buttomSearchByProductName = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.buttonSearchByClientName = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.buttonSearchByMoney = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
             this.buttonViewAll = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonSearchByMoney = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonSearchByClientName = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttomSearchByProductName = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSearchByID = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.orderServiceBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -123,7 +123,7 @@
             this.buttonSerialize.Name = "buttonSerialize";
             this.buttonSerialize.Size = new System.Drawing.Size(145, 50);
             this.buttonSerialize.TabIndex = 6;
-            this.buttonSerialize.Text = "将订单导出为xml文件并退出";
+            this.buttonSerialize.Text = "将订单导出为xml文件";
             this.buttonSerialize.UseVisualStyleBackColor = true;
             this.buttonSerialize.Click += new System.EventHandler(this.buttonSerialize_Click);
             // 
@@ -231,94 +231,24 @@
             this.splitContainer2.SplitterDistance = 125;
             this.splitContainer2.TabIndex = 1;
             // 
-            // label2
+            // buttonViewAll
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 15);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "订单号：";
+            this.buttonViewAll.Location = new System.Drawing.Point(812, 4);
+            this.buttonViewAll.Name = "buttonViewAll";
+            this.buttonViewAll.Size = new System.Drawing.Size(86, 104);
+            this.buttonViewAll.TabIndex = 12;
+            this.buttonViewAll.Text = "显示全部订单";
+            this.buttonViewAll.UseVisualStyleBackColor = true;
+            this.buttonViewAll.Click += new System.EventHandler(this.buttonViewAll_Click);
             // 
-            // textBox1
+            // label5
             // 
-            this.textBox1.Location = new System.Drawing.Point(82, 3);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(573, 25);
-            this.textBox1.TabIndex = 1;
-            // 
-            // buttonSearchByID
-            // 
-            this.buttonSearchByID.Location = new System.Drawing.Point(661, 4);
-            this.buttonSearchByID.Name = "buttonSearchByID";
-            this.buttonSearchByID.Size = new System.Drawing.Size(144, 25);
-            this.buttonSearchByID.TabIndex = 2;
-            this.buttonSearchByID.Text = "按订单号查询";
-            this.buttonSearchByID.UseVisualStyleBackColor = true;
-            this.buttonSearchByID.Click += new System.EventHandler(this.buttonSearchByID_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 34);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 15);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "商品名：";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(82, 29);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(573, 25);
-            this.textBox2.TabIndex = 4;
-            // 
-            // buttomSearchByProductName
-            // 
-            this.buttomSearchByProductName.Location = new System.Drawing.Point(661, 29);
-            this.buttomSearchByProductName.Name = "buttomSearchByProductName";
-            this.buttomSearchByProductName.Size = new System.Drawing.Size(144, 25);
-            this.buttomSearchByProductName.TabIndex = 5;
-            this.buttomSearchByProductName.Text = "按商品名查询";
-            this.buttomSearchByProductName.UseVisualStyleBackColor = true;
-            this.buttomSearchByProductName.Click += new System.EventHandler(this.buttomSearchByProductName_Click);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(82, 56);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(573, 25);
-            this.textBox3.TabIndex = 6;
-            // 
-            // buttonSearchByClientName
-            // 
-            this.buttonSearchByClientName.Location = new System.Drawing.Point(661, 56);
-            this.buttonSearchByClientName.Name = "buttonSearchByClientName";
-            this.buttonSearchByClientName.Size = new System.Drawing.Size(144, 25);
-            this.buttonSearchByClientName.TabIndex = 7;
-            this.buttonSearchByClientName.Text = "按客户名查询";
-            this.buttonSearchByClientName.UseVisualStyleBackColor = true;
-            this.buttonSearchByClientName.Click += new System.EventHandler(this.buttonSearchByClientName_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 61);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 15);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "客户名：";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(82, 83);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(573, 25);
-            this.textBox4.TabIndex = 9;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "金额：";
             // 
             // buttonSearchByMoney
             // 
@@ -330,24 +260,94 @@
             this.buttonSearchByMoney.UseVisualStyleBackColor = true;
             this.buttonSearchByMoney.Click += new System.EventHandler(this.buttonSearchByMoney_Click);
             // 
-            // label5
+            // textBox4
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(9, 88);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 15);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "金额：";
+            this.textBox4.Location = new System.Drawing.Point(82, 83);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(573, 25);
+            this.textBox4.TabIndex = 9;
             // 
-            // buttonViewAll
+            // label4
             // 
-            this.buttonViewAll.Location = new System.Drawing.Point(812, 4);
-            this.buttonViewAll.Name = "buttonViewAll";
-            this.buttonViewAll.Size = new System.Drawing.Size(86, 104);
-            this.buttonViewAll.TabIndex = 12;
-            this.buttonViewAll.Text = "显示全部订单";
-            this.buttonViewAll.UseVisualStyleBackColor = true;
-            this.buttonViewAll.Click += new System.EventHandler(this.buttonViewAll_Click);
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "客户名：";
+            // 
+            // buttonSearchByClientName
+            // 
+            this.buttonSearchByClientName.Location = new System.Drawing.Point(661, 56);
+            this.buttonSearchByClientName.Name = "buttonSearchByClientName";
+            this.buttonSearchByClientName.Size = new System.Drawing.Size(144, 25);
+            this.buttonSearchByClientName.TabIndex = 7;
+            this.buttonSearchByClientName.Text = "按客户名查询";
+            this.buttonSearchByClientName.UseVisualStyleBackColor = true;
+            this.buttonSearchByClientName.Click += new System.EventHandler(this.buttonSearchByClientName_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(82, 56);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(573, 25);
+            this.textBox3.TabIndex = 6;
+            // 
+            // buttomSearchByProductName
+            // 
+            this.buttomSearchByProductName.Location = new System.Drawing.Point(661, 29);
+            this.buttomSearchByProductName.Name = "buttomSearchByProductName";
+            this.buttomSearchByProductName.Size = new System.Drawing.Size(144, 25);
+            this.buttomSearchByProductName.TabIndex = 5;
+            this.buttomSearchByProductName.Text = "按商品名查询";
+            this.buttomSearchByProductName.UseVisualStyleBackColor = true;
+            this.buttomSearchByProductName.Click += new System.EventHandler(this.buttomSearchByProductName_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(82, 29);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(573, 25);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "商品名：";
+            // 
+            // buttonSearchByID
+            // 
+            this.buttonSearchByID.Location = new System.Drawing.Point(661, 4);
+            this.buttonSearchByID.Name = "buttonSearchByID";
+            this.buttonSearchByID.Size = new System.Drawing.Size(144, 25);
+            this.buttonSearchByID.TabIndex = 2;
+            this.buttonSearchByID.Text = "按订单号查询";
+            this.buttonSearchByID.UseVisualStyleBackColor = true;
+            this.buttonSearchByID.Click += new System.EventHandler(this.buttonSearchByID_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 3);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(573, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "订单号：";
             // 
             // Form1
             // 
